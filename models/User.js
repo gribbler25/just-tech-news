c81af31d608231098b22a5c8090a76c6d101a4ab
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-08-16 10:09:46 -0500

    password feature working

--- models/User.js
+++ models/User.js
@@ -2,7 +2,11 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 const bcrypt = require("bcrypt");
 // create our User model
-class User extends Model {}
+class User extends Model {
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 // define table columns and configuration
 User.init(
   {
@@ -69,17 +73,3 @@ User.init(
 );
 
 module.exports = User;
-
-//     // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
-//     // pass in our imported sequelize connection (the direct connection to our database)
-//     sequelize,
-//     // don't automatically create createdAt/updatedAt timestamp fields
-//     timestamps: false,
-//     // don't pluralize name of database table
-//     freezeTableName: true,
-//     // use underscores instead of camel-casing (i.e. `comment_text` and not `commentText`)
-//     underscored: true,
-//     // make it so our model name stays lowercase in the database
-//     modelName: "user",
-//   }
-// );
