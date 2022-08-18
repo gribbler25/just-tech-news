9719b25aaaf0f0417dae432af5f05014fc956887
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-08-17 21:12:21 -0500

    comments displaying with a post routes finished

---
+++ models/Comment.js
@@ -0,0 +1,45 @@
+const { INTEGER } = require("sequelize");
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+      validate: {
+        len: [2],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "comment",
+  }
+);
+
+module.exports = Comment;
