1d00c80ab8a5f6fa7e296d580882d5cc3a38fadc
gribbler25 <98048059+gribbler25@users.noreply.github.com>
2022-08-18 21:11:48 -0500

    duplicate commit

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
-//import sezuelize constructor
 const Sequelize = require("sequelize");
-//load env variables into this connection..
+
 require("dotenv").config();
+
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
   sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASSWORD,
+    process.env.DB_PW,
     {
       host: "localhost",
       dialect: "mysql",
